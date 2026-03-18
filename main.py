b34d90e57639e063e14328e6cf1b2918bf3180ac
ManuelHerrmannRDF <Manuel.Herrmann@rdf.nuernberg.de>
2026-03-18 09:38:20 +0100

    Buttons eingefügt, Registrierung Seite erstellt.

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/registrierung")
+def registrierung():
+    return render_template("registrierung.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
