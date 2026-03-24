96fd1ba8d7699ed8c02fe44b63d4ecf71584fab9
Erika Rohrmeyer <erika.rohrmeyer@rdf.nuernberg.de>
2026-03-24 18:23:26 +0100

    DB Anbindung, User Funktionen

---
+++ data.py
@@ -0,0 +1,59 @@
+import sqlite3
+from pathlib import Path
+import utils
+import time
+
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / 'beatguessr.db'
+
+def get_connection() -> sqlite3.Connection:
+    
+    connection = sqlite3.connect(DB_PATH)
+    connection .row_factory = sqlite3.Row
+    return connection
+
+
+    
+def init_database():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        
+        # create table SONG
+        cursor.execute("CREATE TABLE IF NOT EXISTS song (songid INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, artist_id INTEGER, mp3_bindata BLOB)")
+        
+        # create table ARTIST
+        cursor.execute("CREATE TABLE IF NOT EXISTS artist (artistid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+        
+        # create table USER
+        cursor.execute("CREATE TABLE IF NOT EXISTS user (userid INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, register_date INTEGER)")
+        
+        # create table HINT
+        cursor.execute("CREATE TABLE IF NOT EXISTS hint (hintid INTEGER PRIMARY KEY AUTOINCREMENT, song_id INTEGER, hint_number INTEGER, hint_text TEXT)")
+        
+        # create table GUESS
+        cursor.execute("CREATE TABLE IF NOT EXISTS guess (guessid INTEGER PRIMARY KEY AUTOINCREMENT, song_id INTEGER, user_id INTEGER, seconds INTEGER)")
+        
+        conn.commit()
+        
+        
+def add_user(username, password):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        date_ms = int(time.time()*1000.0)
+        hashed_pw = utils.hash_password(password)
+        cursor.execute(
+            "INSERT INTO user (username, password, register_date) VALUES (?, ?, ?)",
+            (username, hashed_pw, date_ms)
+        )
+
+def get_users():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user")
+        return cursor.fetchall()
+        
+def get_user(username):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM user WHERE username = '{username}'")
+        return cursor.fetchone()
