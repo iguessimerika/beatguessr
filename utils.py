c2fb82f4b16776b7de2f862e3c920bc4fc0b6658
Erika Rohrmeyer <erika.rohrmeyer@rdf.nuernberg.de>
2026-03-25 09:31:20 +0100

    Logs entfernt

--- utils.py
+++ utils.py
@@ -8,7 +8,4 @@ def hash_password(password):
 def check_password(user_entry, db_entry):
     hashed_user_entry = hash_password(user_entry)
     
-    print(f"user pw hash: {hashed_user_entry}")
-    print(f"db pw hash: {db_entry}")
-    
     return db_entry == hashed_user_entry
