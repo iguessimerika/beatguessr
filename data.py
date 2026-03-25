1e9a80d859ef6c89c252bd43fb5ba8fecc235870
Erika Rohrmeyer <erika.rohrmeyer@rdf.nuernberg.de>
2026-03-25 09:16:26 +0100

    login, registierung und logout funktionen erweitert/hinzugefügt, dashboard.html hinzugefügt

--- data.py
+++ data.py
@@ -25,7 +25,7 @@ def init_database():
         cursor.execute("CREATE TABLE IF NOT EXISTS artist (artistid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
         
         # create table USER
-        cursor.execute("CREATE TABLE IF NOT EXISTS user (userid INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, register_date INTEGER)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS user (userid INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, register_date INTEGER, email TEXT)")
         
         # create table HINT
         cursor.execute("CREATE TABLE IF NOT EXISTS hint (hintid INTEGER PRIMARY KEY AUTOINCREMENT, song_id INTEGER, hint_number INTEGER, hint_text TEXT)")
@@ -36,15 +36,18 @@ def init_database():
         conn.commit()
         
         
-def add_user(username, password):
+def add_user(username, password, email):
     with get_connection() as conn:
         cursor = conn.cursor()
         date_ms = int(time.time()*1000.0)
         hashed_pw = utils.hash_password(password)
         cursor.execute(
-            "INSERT INTO user (username, password, register_date) VALUES (?, ?, ?)",
-            (username, hashed_pw, date_ms)
+            "INSERT INTO user (username, password, register_date, email) VALUES (?, ?, ?, ?)",
+            (username, hashed_pw, date_ms, email)
         )
+        
+        conn.commit()
+        
 
 def get_users():
     with get_connection() as conn:
@@ -52,8 +55,16 @@ def get_users():
         cursor.execute("SELECT * FROM user")
         return cursor.fetchall()
         
-def get_user(username):
+def get_user(email):
     with get_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM user WHERE username = '{username}'")
+        cursor.execute(f"SELECT * FROM user WHERE email = '{email}'")
         return cursor.fetchone()
+    
+if __name__ == "__main__":
+    users = get_users()
+    
+    for user in users:
+        print(f"Name: {user['username']}")
+        
+    print(get_user("test@test.de")['password'])
